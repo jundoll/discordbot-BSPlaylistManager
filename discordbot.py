82efa11b395788ecb4094584c195b5048918f21f
jundoll <jundoll33@gmail.com>
2020-10-24 21:33:12 +0900

    change requiremetn

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,13 @@
 
 # load modules
 import dropbox
+from urllib import request
+from lxml import html
 from discord.ext import commands
 import os
 import traceback
+import hashlib
+import json
 
 
 # init settings
