6463176cabe230cc1bac469d76887a78326707b0
jundoll <jundoll33@gmail.com>
2020-12-30 17:50:30 +0900

    あd

--- DomainService/PlaylistService.py
+++ DomainService/PlaylistService.py
@@ -3,7 +3,7 @@
 import base64
 from urllib.request import urlopen
 import inject
-from Domain.Playlist import ImageBase64, Playlist, PlaylistInfo
+from Domain.Playlist import ImageBase64, Playlist, PlaylistTitle
 from Repository.PlaylistRepository import IPlaylistRepository
 
 
@@ -19,6 +19,43 @@ class PlaylistService:
         # type check
         if not isinstance(self.playlistRepository, IPlaylistRepository):
             raise Exception
+
+    # ---------------------
+
+    # タイトルからプレイリストを取得する
+    def findPlaylist(self, playlistTitle: PlaylistTitle) -> Playlist:
+
+        # type check
+        if not isinstance(playlistTitle, PlaylistTitle):
+            raise Exception
+
+        # タイトルからプレイリストを取得する
+        playlist = self.playlistRepository.readPlaylist(playlistTitle)
+        if playlist is None:
+            raise Exception("指定のプレイリストが見つからないよ！")
+            # エラーケース
+            # ・プレイリスト情報一覧が見つからない
+            # ・プレイリスト情報一覧に一致するタイトルが見つからない
+            # ・タイトルに一致するプレイリストが見つからない
+        else:
+            return playlist
+
+    # ------------------------
+
+    # 空のプレイリストを新規作成する
+    def create(self, playlist: Playlist):
+
+        # type check
+        if not isinstance(playlist, Playlist):
+            raise Exception
+
+        # save info
+        self.playlistRepository.registerInfo(playlist.playlistInfo)
+
+        # save playlist
+        self.playlistRepository.savePlaylist(playlist)
+
+    # ------------------------
 
     # 画像をbase64変換する
     def encodeImage(self, imageUrl: str = "") -> ImageBase64:
@@ -40,22 +77,6 @@ class PlaylistService:
         else:
             raise Exception("png または jpg(jpeg) ファイルを指定してね！")
         return image
-
-    # 空のプレイリストを新規作成する
-    def create(self, playlist: Playlist):
-
-        # depulicated check
-        if self._exists(playlist.playlistInfo):
-            raise Exception("既に存在するタイトルだよ！違う名前に変えてね！")
-
-        # save
-        self.playlistRepository.create(playlist)
-
-    # 指定タイトルのプレイリストが存在するかどうか
-    def _exists(self, playlistInfo: PlaylistInfo) -> bool:
-        foundPlaylist = self.playlistRepository.findPlaylistByTitle(
-            playlistInfo.playlistTitle)
-        return foundPlaylist is not None
 
     # ------------------------
 
