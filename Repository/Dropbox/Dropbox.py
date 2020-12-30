ef6a30d16b7c7596b2c5623c356ed4ad369f662e
jundoll <jundoll33@gmail.com>
2020-12-30 17:54:08 +0900

    ファイルアクセス方法を大幅修正 infolistもjson化

--- Repository/Dropbox/Dropbox.py
+++ Repository/Dropbox/Dropbox.py
@@ -1,10 +1,8 @@
 
 # load modules
-import csv
 import json
 import os
 from typing import List, Union
-from urllib import request
 import dropbox
 from Domain.Playlist import ImageBase64, Playlist, PlaylistAuthor, PlaylistDescription, PlaylistFileName, PlaylistId, PlaylistInfo, PlaylistTitle
 from Domain.Song import Song
@@ -24,123 +22,167 @@ class Dropbox:
     def __init__(self):
         self.dbx = dropbox.Dropbox(dropbox_token)
         self.dpathInfoList = "/{}/PlaylistInfoList".format(dropbox_path)
-        self.fpathInfoList = "{}/infoList.csv".format(self.dpathInfoList)
+        self.fpathInfoList = "{}/infoList.json".format(self.dpathInfoList)
         self.dpathPlaylist = "/{}/Playlists".format(dropbox_path)
 
-        # make playlist folder if the folder does not exists
-        entryList = self._findPathList(self.dpathPlaylist)
-        if len(entryList) == 0:
-            self.dbx.files_create_folder_v2(self.dpathPlaylist)
+    # ---------------------
 
-    # タイトルからプレイリスト情報を取得する
-    def findInfoByTitle(self, playlistTitle: PlaylistTitle) -> Union[PlaylistInfo, None]:
+    # タイトルからプレイリスト情報を読み込む
+    def readInfoByTitle(self, playlistTitle: PlaylistTitle) -> Union[PlaylistInfo, None]:
 
-        # read info list
-        infoList = self._readInfoList()
+        # プレイリスト情報一覧を読み込む
+        infoList = self.readInfoListByTitle()
 
-        # find info by title
-        if infoList is not None:
-            for info in infoList:
-                if info.playlistTitle == playlistTitle:
-                    return info
+        # タイトルからプレイリスト情報を取得する
+        for info in infoList:
+            if info.playlistTitle == playlistTitle:
+                return info
+
+    # ---------------------
 
     # プレイリスト情報一覧を読み込む
-    def _readInfoList(self) -> Union[List[PlaylistInfo], None]:
+    def readInfoListByTitle(self) -> Union[List[PlaylistInfo], None]:
 
-        # read info list
-        infoList = []
+        # プレイリスト情報一覧を json 形式で読み込む
+        infoListJson = self._readJsonFile(self.fpathInfoList)
+        if infoListJson is None:
+            return
+
+        # プレイリスト情報一覧を json 形式から変換する
+        infoList = self._convertInfoListFromJson(infoListJson)
+
+        # return
+        return infoList
+
+    # json 形式のファイルを読み込む
+    def _readJsonFile(self, filePath: str) -> Union[dict, None]:
+
+        # exist check
+        pathList = self._findPathList(filePath)
+        if len(pathList) == 0:
+            return
+
+        # json 形式のファイルを取得する
         try:
-            with open(self.fpathInfoList) as f:
-                reader = csv.DictReader(f)
-                for row in reader:
-                    info = self._convertInfoListFromJson(row)
-                    infoList += [info]
-        except FileNotFoundError:
-            self._createInfoList()
-            return
+            metadata, res = self.dbx.files_download(filePath)
+        except Exception as e:
+            print(e)
+            raise Exception("ファイルの取得に失敗したよ！")
 
-        # if the file is empty file
-        if len(infoList) == 0:
-            self._createInfoList()
-            return
-        else:
-            return infoList
+        # json 形式のファイルを読み込む
+        readJson = json.loads(res.content)
+        return readJson
 
     # 指定の文字列を含むフォルダ・ファイルパスの一覧を返す
-    def _findPathList(self, keyword: str, entryList: list = list()) -> List[str]:
+    def _findPathList(self, keyword: str) -> List[str]:
+
+        # 指定の文字列を含むフォルダ・ファイルパスの一覧を取得する
         res = self.dbx.files_list_folder("", recursive=True)
-        entryList += [
-            entry for entry in res.entries if (keyword in entry.path_display)]
+        entryList = [] + \
+            [entry.path_lower for entry in res.entries if (
+                keyword in entry.path_display)]
+
+        # return
         return entryList
 
-    # プレイリスト情報一覧を新規作成する
-    def _createInfoList(self):
+    # プレイリスト情報一覧を json 形式から変換する
+    def _convertInfoListFromJson(self, infoListJson: dict) -> List[PlaylistInfo]:
 
-        # make info list folder if the folder does not exists
+        # プレイリスト情報一覧に特定のキーが存在するかどうかのチェック
+        infoListKeys = ["infoList"]
+        if set(infoListJson.keys()) != set(infoListKeys):
+            raise Exception("プレイリスト情報一覧の読み込み中にエラーが発生したよ！")
+
+        # 取得した元のプレイリスト情報一覧を変換する
+        infoList = []
+        for infoJson in infoListJson["infoList"]:
+
+            # プレイリスト情報に特定のキーが存在するかどうかのチェック
+            infoKeys = ["ID", "FileName", "Title"]
+            if set(infoJson.keys()) != set(infoKeys):
+                raise Exception("プレイリスト情報の読み込み中にエラーが発生したよ！")
+
+            # 取得した元のプレイリスト情報を変換する
+            info = PlaylistInfo(
+                playlistId=PlaylistId(infoJson["ID"]),
+                playlistFileName=PlaylistFileName(infoJson["FileName"]),
+                playlistTitle=PlaylistTitle(infoJson["Title"])
+            )
+
+            # プレイリスト情報をリストに追加する
+            infoList += [info]
+
+        # return
+        return infoList
+
+    # ----------------------------------
+
+    # プレイリスト情報を保存する
+    def saveInfoList(self, playlistInfoList: List[PlaylistInfo]):
+
+        # 格納フォルダがなければ作成する
         entryList = self._findPathList(self.dpathInfoList)
-        raise Exception(len(entryList))
         if len(entryList) == 0:
             self.dbx.files_create_folder_v2(self.dpathInfoList)
 
-        # create info list file
-        fileList = self._findPathList(self.fpathInfoList)
-        raise Exception(fileList)
-        if len(fileList) == 0:
-            with open(self.fpathInfoList, "x") as f:
-                writer = csv.DictWriter(f, ["ID", "FileName", "Title"])
-                writer.writeheader()
+        # プレイリスト情報一覧を json 形式に変換する
+        playlistInfoListJson = {"infoList": []}
+        for info in playlistInfoList:
+            infoDict = {
+                "ID": info.playlistId,
+                "FileName": info.playlistFileName,
+                "Title": info.playlistTitle
+            }
+            playlistInfoListJson["infoList"].append(infoDict)
 
-    # プレイリスト情報一覧を json 形式から変換する
-    def _convertInfoListFromJson(self, infoListJson: dict) -> PlaylistInfo:
+        # json 形式のプレイリスト情報一覧を保存する
+        self._saveJsonFile(playlistInfoListJson, self.fpathInfoList)
 
-        # key exists check
-        infoListKeys = ["ID", "FileName", "Title"]
-        if set(infoListJson.keys()) != infoListKeys:
-            raise Exception("プレイリスト情報一覧の読み込み中にエラーが発生したよ！")
+    # json 形式のファイルを保存する
+    def _saveJsonFile(self, jsonData: dict, filePath: str):
 
-        # get info
-        playlistInfo = PlaylistInfo(
-            playlistId=PlaylistId(infoListJson["ID"]),
-            playlistFileName=PlaylistFileName(infoListJson["FileName"]),
-            playlistTitle=PlaylistTitle(infoListJson["Title"])
-        )
-        return playlistInfo
+        # dump する
+        jsonDataBytes = bytes(json.dumps(jsonData,
+                                         separators=(',', ':'), ensure_ascii=False), encoding="utf-8")
+
+        # json 形式のファイルを保存する
+        try:
+            self.dbx.files_upload(jsonDataBytes, filePath,
+                                  mode=dropbox.files.WriteMode.overwrite)
+        except Exception:
+            raise Exception("ファイルの保存に失敗したよ！")
 
     # ----------------------------------
 
     # プレイリストを読み込む
-    def readPlaylistJsonByFileName(self, playlistFileName: PlaylistFileName) -> Playlist:
+    def readPlaylist(self, playlistInfo: PlaylistInfo) -> Union[Playlist, None]:
 
-        # set file path
+        # ファイルパスを指定する
         filePath = "{}/{}.json".format(self.dpathPlaylist,
-                                       playlistFileName.playlistFileName)
+                                       playlistInfo.playlistFileName.playlistFileName)
 
-        # load playlist
-        playlistJson = request.urlopen(filePath).read()
-        playlistJson = json.loads(playlistJson)
+        # プレイリストを json 形式で読み込む
+        playlistJson = self._readJsonFile(filePath)
+        if playlistJson is None:
+            return
 
-        # convert playlist from dict(json)
-        playlist = self._convertPlaylistFromJson(playlistJson)
+        # プレイリストを json 形式から変換する
+        playlist = self._convertPlaylistFromJson(playlistJson, playlistInfo)
         return playlist
 
     # プレイリストを json 形式から変換する
-    def _convertPlaylistFromJson(self, playlistJson: dict) -> Playlist:
+    def _convertPlaylistFromJson(self, playlistJson: dict, playlistInfo: PlaylistInfo) -> Union[Playlist, None]:
 
-        # key exists check
+        # プレイリストに特定のキーが存在するかどうかのチェック
         playlistKeys = ["playlistTitle", "playlistAuthor",
                         "playlistDescription", "image", "songs"]
-        if set(playlistJson.keys()) != playlistKeys:
+        if set(playlistJson.keys()) != set(playlistKeys):
             raise Exception("プレイリストの読み込み中にエラーが発生したよ！")
 
-        # get info
-        playlistInfo = self.findInfoByTitle(playlistJson["playlistTitle"])
-        if playlistInfo is None:
-            raise Exception("指定のプレイリストが見つからないよ！")
-
-        # convert
+        # 取得した元のプレイリストを変換する
         playlist = Playlist(
             playlistInfo=playlistInfo,
-            playlistTitle=playlistJson["playlistTitle"],
+            playlistTitle=playlistInfo.playlistTitle,
             playlistAuthor=PlaylistAuthor(playlistJson["playlistAuthor"]),
             playlistDescription=PlaylistDescription(
                 playlistJson["playlistDescription"]),
@@ -154,13 +196,18 @@ class Dropbox:
     # ----------------------------------
 
     # プレイリストを保存する
-    def savePlaylistJson(self, playlist: Playlist):
+    def savePlaylist(self, playlist: Playlist):
 
-        # set file path
+        # ファイルパスを指定する
         filePath = "{}/{}.json".format(self.dpathPlaylist,
                                        playlist.playlistInfo.playlistFileName.playlistFileName)
 
-        # convert playlist into dict(json)
+        # 格納フォルダがなければ作成する
+        entryList = self._findPathList(self.dpathPlaylist)
+        if len(entryList) == 0:
+            self.dbx.files_create_folder_v2(self.dpathPlaylist)
+
+        # プレイリストを json 形式に変換する
         playlistJson = {
             "playlistTitle": playlist.playlistTitle.playlistTitle,
             "playlistAuthor": playlist.playlistAuthor.playlistAuthor,
@@ -169,142 +216,75 @@ class Dropbox:
             "songs": [song.convertJson() for song in playlist.songs]
         }
 
-        # update playlist
-        playlistBytes = bytes(json.dumps(playlistJson, separators=(
-            ',', ':'), ensure_ascii=False), encoding="utf-8")
-        self.dbx.files_upload(playlistBytes, filePath,
-                              mode=dropbox.files.WriteMode.overwrite)
+        # json 形式のプレイリストを保存する
+        self._saveJsonFile(playlistJson, filePath)
 
     # ----------------------------------
 
-    # 指定のプレイリスト情報を情報一覧に追加する
-    # playlistのように一度読み込んで書き込む方がスマートかも？
-    def registerInfo(self, playlistInfo: PlaylistInfo):
+    # タイトルからプレイリストを削除する
+    def deletePlaylist(self, playlistTitle: PlaylistTitle):
 
-        # read info list
-        infoList = self._readInfoList()
+        # タイトルからプレイリスト情報を取得する
+        playlistInfo = self.readInfoByTitle(playlistTitle)
+        if playlistInfo is None:
+            raise Exception("指定のプレイリストが見つからないよ！")
+            # エラーケース
+            # ・プレイリスト情報一覧が見つからない
+            # ・プレイリスト情報一覧に一致するタイトルが見つからない
 
-        # duplicated check for Id and FileName and Title
-        if infoList is not None:
-            if playlistInfo.playlistId in [info.playlistId for info in infoList]:
-                raise Exception
-            if playlistInfo.playlistFileName in [info.playlistFileName for info in infoList]:
-                raise Exception
-            if playlistInfo.playlistTitle in [info.playlistTitle for info in infoList]:
-                raise Exception
-
-        # convert info into dict
-        infoDict = {
-            "ID": playlistInfo.playlistId,
-            "FileName": playlistInfo.playlistFileName,
-            "Title": playlistInfo.playlistTitle
-        }
-
-        # register info
-        try:
-            with open(self.fpathInfoList, "a") as f:
-                writer = csv.DictWriter(f, ["ID", "FileName", "Title"])
-                writer.writerow(infoDict)
-        except FileNotFoundError:
-            self._createInfoList()
-
-    # ----------------------------------
-
-    def unregisterInfo(self, playlistInfo: PlaylistInfo, playlistFileName: PlaylistFileName = PlaylistFileName("infoList")):
-
-        # read info list
-        infoList = self._readInfoList()
-
-        # unregister info
-        infoList.remove(playlistInfo)
-
-        # unregister info
-        with open(self.fpathInfoList, "w") as f:
-            writer = csv.DictWriter(f, ["ID", "FileName", "Title"])
-            writer.writeheader()
-            for info in infoList:
-                infoDict = {
-                    "ID": info.playlistId,
-                    "FileName": info.playlistFileName,
-                    "Title": info.playlistTitle
-                }
-                writer.writerow(infoDict)
-
-    def deletePlaylist(self, playlistInfo: PlaylistInfo):
-
-        # set file path
+        # ファイルパスを指定する
         filePath = "{}/{}.json".format(self.dpathPlaylist,
                                        playlistInfo.playlistFileName.playlistFileName)
 
-        # delete playlist
-        self.dbx.files_delete(filePath)
+        # 格納フォルダがなければ作成する
+        entryList = self._findPathList(self.dpathPlaylist)
+        if len(entryList) == 0:
+            self.dbx.files_create_folder_v2(self.dpathPlaylist)
+            return
+
+        # プレイリストを削除する
+        try:
+            self.dbx.files_delete(filePath)
+        except Exception:
+            raise Exception("プレイリストの削除に失敗したよ！")
 
     # ----------------------------------
 
-    def getSharedLink(self, playlistTitle: PlaylistTitle) -> str:
+    # タイトルからプレイリストのダウンロードURLを取得する
+    def getSharedLink(self, playlistTitle: PlaylistTitle) -> Union[str, None]:
 
-        # find info by title
-        info = self.findInfoByTitle(playlistTitle)
-        if info is None:
+        # タイトルからプレイリスト情報を取得する
+        playlistInfo = self.readInfoByTitle(playlistTitle)
+        if playlistInfo is None:
             raise Exception("指定のプレイリストが見つからないよ！")
+            # エラーケース
+            # ・プレイリスト情報一覧が見つからない
+            # ・プレイリスト情報一覧に一致するタイトルが見つからない
 
-        # set file path
+        # ファイルパスを指定する
         filePath = "{}/{}.json".format(self.dpathPlaylist,
-                                       info.playlistFileName.playlistFileName)
+                                       playlistInfo.playlistFileName.playlistFileName)
 
-        # get link
+        # 共有リンクを取得する
         links = self.dbx.sharing_list_shared_links(
-            path=self.fpath, direct_only=True).links
+            path=filePath, direct_only=True).links
 
-        if links is not None:
-            for link in links:
-                return link.url.replace("dl=0", "dl=1")
+        if links is None:
+            # 共有リンクを発行する
+            links = [self._createSharedLink(filePath)]
+
+        if len(links) == 0:
             raise Exception("リンクの取得に失敗したよ！")
         else:
-            return self._createSharedLink(filePath)
+            for link in links:
+                return link.url.replace("dl=0", "dl=1")
 
+    # 共有リンクを発行する
     def _createSharedLink(self, filePath: str) -> str:
 
-        # create shared link
+        # 共有リンクを発行する
         setting = dropbox.sharing.SharedLinkSettings(
             requested_visibility=dropbox.sharing.RequestedVisibility.public)
         link = self.dbx.sharing_create_shared_link_with_settings(
             path=filePath, settings=setting)
-        return link.url.replace("dl=0", "dl=1")
-
-    # ----------------------------------
-
-    # 指定のパスが存在するかどうかを返す
-    def _fileExistsInfoList(self, filePath: str) -> bool:
-        fileList = self._findPathList(filePath)
-        return len(fileList) == 0
-
-    def makePlaylistPath(self):
-        pass
-
-    def saveInfoList(self, filePath: str, info: dict, isNew: bool = False):
-        if isNew:
-            # 新規作成
-            with open(filePath, "w") as f:
-                writer = csv.DictWriter(f, ["ID", "FileName", "Title"])
-                writer.writeheader()
-        else:
-            # 既存追加
-            with open(filePath, "a") as f:
-                writer = csv.DictWriter(f, ["ID", "FileName", "Title"])
-                writer.writerow(info)
-
-    def getFileNameById(self, id: PlaylistId) -> Union[PlaylistFileName, None]:
-        infoList = self.readInfoList()
-        if len(infoList) == 0:
-            pass
-        else:
-            for info in infoList:
-                if info.playlistId == id:
-                    return info.playlistFileName
-
-    def getFilePathList(self) -> List:
-        res = self.dbx.files_list_folder("", recursive=True)
-        for entry in res.entries:
-            if entry.path_display.startswith("/"+dropbox_path+"/"):
-                self.fpath = entry.path_lower
+        return link
